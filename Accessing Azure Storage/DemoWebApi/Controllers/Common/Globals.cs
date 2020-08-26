41e2572f33888502cea1ee7f8093c727cd32b86f
Giulio Pagan <giulio.pagan@autodesk.com>
2020-08-26 19:58:47 +0100

    first with gitignore

---
+++ Accessing Azure Storage/DemoWebApi/Controllers/Common/Globals.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoWebApi.Controllers.Common
+{
+    public class Globals
+    {
+        /// <summary>
+        /// Reads appsettings from web.config
+        /// </summary>
+        public static string GetAppSetting(string settingKey)
+        {
+            return Environment.GetEnvironmentVariable(settingKey);
+        }
+    }
+}
