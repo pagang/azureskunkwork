fadf412b3abe5ed097765b21486b4398f37604ed
Giulio Pagan <giulio.pagan@autodesk.com>
2020-08-26 09:10:41 +0100

    first version

---
+++ Accessing Azure Storage/DemoAzureStorageExplorer/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace DemoAzureStorageExplorer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnReadLog = new System.Windows.Forms.Button();
+            this.logBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnReadLog
+            // 
+            this.btnReadLog.Location = new System.Drawing.Point(12, 12);
+            this.btnReadLog.Name = "btnReadLog";
+            this.btnReadLog.Size = new System.Drawing.Size(75, 23);
+            this.btnReadLog.TabIndex = 0;
+            this.btnReadLog.Text = "Read Log";
+            this.btnReadLog.UseVisualStyleBackColor = true;
+            this.btnReadLog.Click += new System.EventHandler(this.btnReadLog_Click);
+            // 
+            // logBox
+            // 
+            this.logBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.logBox.FormattingEnabled = true;
+            this.logBox.ItemHeight = 15;
+            this.logBox.Location = new System.Drawing.Point(12, 41);
+            this.logBox.Name = "logBox";
+            this.logBox.Size = new System.Drawing.Size(776, 394);
+            this.logBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.logBox);
+            this.Controls.Add(this.btnReadLog);
+            this.Name = "Form1";
+            this.Text = "Azure Table Storage Testbed";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReadLog;
+        private System.Windows.Forms.ListBox logBox;
+    }
+}
+
---
+++ Accessing Azure Storage/DemoAzureStorageExplorer/Form1.cs
@@ -0,0 +1,36 @@
+﻿using AzureUtils;
+using AzureUtils.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoAzureStorageExplorer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnReadLog_Click(object sender, EventArgs e)
+        {
+            AzureStorageManager azStorageMgr = new AzureStorageManager("DefaultEndpointsProtocol=https;AccountName=bdpstoacc;AccountKey=cHiG1TBtuOnHHmf+vLJgQBTxhVqSmELqkbYnGjDk9bgtFP1TJZr39F9RJtfK4HTJPtuP9fDDwgQuh44tU6tIGQ==;EndpointSuffix=core.windows.net", "BdpFamilySanitizer");
+            DesignAutomationRecord[] dars = await azStorageMgr.ReadAutomationRecord("DesignAutomationLog");
+
+            logBox.Items.Clear();
+            foreach(DesignAutomationRecord dar in dars)
+            {
+                logBox.Items.Add($"{dar.WorkItemId}  {dar.InputModelName} { dar.InputSource} {dar.WorkItemStatus}");
+            }
+
+
+        }
+    }
+}
